ddd5c6e51979f2d875bc9dfc6b27b1911f32200b
craig <none@none>
2006-08-28 09:14:59 +0000

    New Flickr badge

--- web/southesk/www/templates/southesk1/index.php
+++ web/southesk/www/templates/southesk1/index.php
@@ -17,27 +17,6 @@ if ( $my->id ) {
 <?php mosShowHead(); ?>
 <link rel="shortcut icon" href="<?php echo $mosConfig_live_site;?>/images/favicon.ico" />
 <link rel="stylesheet" type="text/css" href="<?php echo $mosConfig_live_site; ?>/templates/southesk1/css/template_css.css" />
-
-        <style type="text/css">
-        .zg_div {margin:0px 5px 5px 0px; width:117px;}
-        .zg_div_inner {border: solid 1px #000000; background-color:#ffffff;  color:#666666; text-align:center; font-family:arial, helvetica; font-size:11px;}
-        .zg_div a, .zg_div a:hover, .zg_div a:visited {color:#D20466; background:inherit !important; text-decoration:none !important;}
-        </style>
-        <script type="text/javascript">
-        zg_insert_badge = function() {
-        var zg_bg_color = 'ffffff';
-        var zgi_url = 'http://www.flickr.com/apps/badge/badge_iframe.gne?zg_bg_color='+zg_bg_color+'&zg_person_id=97149645%40N00';
-        document.write('<iframe style="background-color:#'+zg_bg_color+'; border-color:#'+zg_bg_color+'; border:none;" width="113" height="151" frameborder="0" scrolling="no" src="'+zgi_url+'" title="Flickr Badge"></iframe>');
-        if (document.getElementById) document.write('<div id="zg_whatlink"><a href="http://www.flickr.com/badge_new.gne"  style="color:#D20466;" onclick="zg_toggleWhat(); return false;">what is this?<\/a><\/div>');
-        }
-        zg_toggleWhat = function() {
-        document.getElementById('zg_whatdiv').style.display = (document.getElementById('zg_whatdiv').style.display != 'none') ? 'none' : 'block';
-        document.getElementById('zg_whatlink').style.display = (document.getElementById('zg_whatdiv').style.display != 'none') ? 'none' : 'block';
-        return false;
-        }
-        </script>
-
-
 </head>
 <body>
 
@@ -90,12 +69,7 @@ if ( $my->id ) {
         <tr>
         <td align=center>
         <!-- Start of Flickr Badge -->
-        <div class="zg_div"><div class="zg_div_inner"><a href="http://www.flickr.com">www.<strong style="color:#3993ff">flick<span style="color:#ff1c92">r</span></strong>.com</a><br>
-        <script type="text/javascript">zg_insert_badge();</script>
-        <div id="zg_whatdiv">This is a Flickr badge showing public photos from <a href="http://www.flickr.com/photos/97149645@N00">CraigMcKay</a>. Make your own badge <a href="http://www.flickr.com/badge_new.gne">here</a>.</div>
-        <script type="text/javascript">if (document.getElementById) document.getElementById('zg_whatdiv').style.display = 'none';</script>
-        </div>
-        </div>
+        <a href="http://www.flickr.com/photos/mcmoose/"><img src="http://flagrantdisregard.com/flickr/profilewidget/recent.vert/ffffff/d70042/97149645@N00.jpg" border="0" alt="Craig McKay&#039;s photos" title="Craig McKay&#039;s photos" /></a>
         <!-- End of Flickr Badge -->
         </td>
         </tr>

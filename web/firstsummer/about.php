94bb9cc79e49cc7f7cef7a9f46cb3ba19ef23b43
craig <none@none>
2006-10-04 08:28:41 +0000

    10003

---
+++ web/firstsummer/about.php
@@ -0,0 +1,31 @@
+<?
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * First Summerhouses
+ * Copyright (c) 2006 Frontburner
+ * Author Craig McKay <craig@frontburner.co.uk>
+ *
+ * About Us page
+ *
+ * $Id: contact.php 199 2006-07-14 15:51:13Z craig $
+ *
+ * Who  When         Why
+ * CAM  02-Oct-2006  10003 : File created.
+ * * * * * * * * * * * * * * * * * * * * * * * */
+
+$title = "About Us";
+include 'tpl/top.php';
+?>
+
+<table height="100%" width="100%" border=0 cellpadding=10 cellspacing=0>
+<tr>
+  <td align=center>
+    <h3>1st Summerhouses is a family business based near Glasgow.</h3>
+
+    <p>We have established a distribution network that delivers throughout the UK and Europe.
+    We pride ourselves in our friendly, personal service and being able to offer our customers value for money.</p></td>
+</tr>
+</table>
+
+<?
+include 'tpl/bot.php';
+?>

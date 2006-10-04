065e591ed226612966d0da36b9797c3925bc337d
craig <none@none>
2006-10-04 09:04:09 +0000

    10044

---
+++ web/firstsummer/resellers.php
@@ -0,0 +1,42 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * First Summerhouses
+ * Copyright (c) 2006 Frontburner
+ * Author Craig McKay <craig@frontburner.co.uk>
+ *
+ * Resellers page
+ *
+ * $Id: category.php 184 2006-07-05 00:15:13Z craig $
+ *
+ * Who  When         Why
+ * CAM  02-Oct-2006  10044 : File created.
+ * * * * * * * * * * * * * * * * * * * * * * * */
+
+include_once 'Main.php';
+
+$title = "Resellers";
+include 'tpl/top.php';
+?>
+<table border=0 cellpadding=10 cellspacing=0 width="100%">
+
+<tr>
+
+  <td width="100%" valign=top><h3>Reseller</h3>
+
+  <p>Address and contact details</p></td>
+
+  <td rowspan=2 align=right><img src="img/ukresellers.gif"></td>
+
+</tr>
+
+<tr>
+
+  <td valign=top>products supported go here</td>
+
+</tr>
+
+</table>
+
+<?
+include 'tpl/bot.php';
+?>

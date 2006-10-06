38a050b044a291a8f5861ef40c3e878aa2096196
craig <none@none>
2006-10-06 20:47:23 +0000

    10054

--- web/relaxatub/data/data.sql
+++ web/relaxatub/data/data.sql
@@ -9,13 +9,13 @@
 -- SET PASSWORD FOR 'relaxatub'@'localhost' = OLD_PASSWORD('hazel44');
 -- --------------------------------------------------------
 -- phpMyAdmin SQL Dump
--- version 2.8.1
+-- version 2.9.0-rc1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 29, 2006 at 01:55 AM
--- Server version: 4.1.11
--- PHP Version: 4.4.1
+-- Generation Time: Oct 06, 2006 at 09:54 PM
+-- Server version: 4.1.16
+-- PHP Version: 5.1.1
 --
 -- Database: relaxatub_com_-_prod
 -- --------------------------------------------------------
@@ -30,13 +30,14 @@ CREATE TABLE acc (
   dispOrder int(11) NOT NULL default '0',
   photo varchar(100) default '0',
   PRIMARY KEY  (acc)
-) ENGINE=MyISAM  COMMENT='Accessories';
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Accessories';
 
 --
 -- Dumping data for table acc
 --
 
-INSERT INTO acc (acc, accName, dispOrder, photo) VALUES (94889, 'Lift''n''Store', 100, '94889.jpg'),
+INSERT INTO acc (acc, accName, dispOrder, photo) VALUES
+(94889, 'Lift''n''Store', 100, '94889.jpg'),
 (95086, 'Slide''n''Store', 110, '95086.jpg'),
 (94888, 'Swingbrella', 120, 'swing1.jpg'),
 (94891, 'Drinks Bar complete with 2 Stools', 130, NULL),
@@ -63,13 +64,14 @@ CREATE TABLE faq (
   question mediumtext NOT NULL,
   answer mediumtext NOT NULL,
   PRIMARY KEY  (faqno)
-) ENGINE=MyISAM  COMMENT='FAQ';
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='FAQ';
 
 --
 -- Dumping data for table faq
 --
 
-INSERT INTO faq (faqno, summary, question, answer) VALUES (1, 'advanced design', 'What makes Beachcomber''s Protec Hot Tub so Different?', 'Beachcomber''s Protec Hot Tub is more comfortable, more energy efficient and safter.  These three advantages set Protec apart from all other Hot Tubs.  How do we do it?\n<ul>\n<li class="faqa">First, we move the motor and pump outside the tub cavity.  This creates even more room inside the Protec Hot Tub for you to get comfortable.</li>\n<li class="faqa">It also made it possible to take the second step - insulating all four sides of the Hot Tub (and every other model of portable Beachcomber Hot Tub) just like you would in the most energy efficient home today.  Every nook and cranny is filled with insulation to make Beachcomber''s Protec Hot Tub the most energy efficient Hot Tub available today.</li>\n<li class="faqa">Third, we designed a housing for the pump and motor to do double duty as a step that makes it easier for people to get in and out of the tub safely.  The special housing also makes it easier to access the pump and motor.  We also have an injection system that draws air from the pump and motor housing into mixing ducts, injecting warm air instead of could into the water.  Protec really is something different.</li>\n</ul>'),
+INSERT INTO faq (faqno, summary, question, answer) VALUES
+(1, 'advanced design', 'What makes Beachcomber''s Protec Hot Tub so Different?', 'Beachcomber''s Protec Hot Tub is more comfortable, more energy efficient and safter.  These three advantages set Protec apart from all other Hot Tubs.  How do we do it?\n<ul>\n<li class="faqa">First, we move the motor and pump outside the tub cavity.  This creates even more room inside the Protec Hot Tub for you to get comfortable.</li>\n<li class="faqa">It also made it possible to take the second step - insulating all four sides of the Hot Tub (and every other model of portable Beachcomber Hot Tub) just like you would in the most energy efficient home today.  Every nook and cranny is filled with insulation to make Beachcomber''s Protec Hot Tub the most energy efficient Hot Tub available today.</li>\n<li class="faqa">Third, we designed a housing for the pump and motor to do double duty as a step that makes it easier for people to get in and out of the tub safely.  The special housing also makes it easier to access the pump and motor.  We also have an injection system that draws air from the pump and motor housing into mixing ducts, injecting warm air instead of could into the water.  Protec really is something different.</li>\n</ul>'),
 (2, 'guarantee', 'What do you mean by your Guarantee versus the Hot Tub Industry''s Limited Warranty?', 'The industry''s standard Limited Warranty protects the manufacturer and not the customer.  Buying a Hot Tub is a major purchase, but few people understand what can go wrong, and are not as familiar with buying a Hot Tub as they are with other household appliciances.  Beachcomber''s guarantee protects <i>you</i>, the customer, and enables us to protect you after you purchase a Hot Tub.  It is still a young industry and we recommend that you get a guarantee like Beachcomber''s when you buy a Hot Tub, not the industry''s standard Limited Warranty.'),
 (3, 'control & maintenance', 'Is it hard to keep a Hot Tub running properly?', 'It could be if it didn''t have Beachcomber''s Management Centre.  Every Beachcomber Hot Tub has a user-friendly control pad installed on the entrance lip to let you manage the heating and cleaning functions automatically so your Hot Tub is always ready for use when you are.  This computerised system lets you control the maintenance of your Beachcomber, and saves you money in energy costs on many models.  There is even a special mode for when you are away, or for your Hot Tub at the cottage or second home.  You control it with the touch of a finger: nothing could be simpler, or more energy efficient.'),
 (4, 'service', 'What can a family business do for me that a big corporation can''t?', 'A family business like Beachcomber can give you the service with a personal touch that a big corporation simply cannot do.  The portable tub and spa industry concept is only about 25 years old.  We need pioneering entrepreneurs participating directly in their companies at this early stage.  Imagine if the founders of the automobile, pioneers like Benz of Mercedes and Henry Ford, sold to conglomerates early on in their business cycle.  It is too soon for Hot Tub makers to start servicing the shareholders for the best return on their investment.  Beachcomber wants to build a Hot Tub that gives the retail customer the best quality, price and service for their investment.  Keith Scott has done just this every day since he started 20 years ago.'),
@@ -90,13 +92,14 @@ CREATE TABLE photo (
   location varchar(100) default '',
   tub varchar(100) default '',
   PRIMARY KEY  (photo)
-) ENGINE=MyISAM  COMMENT='Photo Album';
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Photo Album';
 
 --
 -- Dumping data for table photo
 --
 
-INSERT INTO photo (photo, location, tub) VALUES ('p001.jpg', 'Aberdeen', NULL),
+INSERT INTO photo (photo, location, tub) VALUES
+('p001.jpg', 'Aberdeen', NULL),
 ('p002.jpg', 'Aberdeen', NULL),
 ('p003.jpg', 'Aberdeen', NULL),
 ('p004.jpg', 'Aberdeen', NULL),
@@ -117,7 +120,11 @@ INSERT INTO photo (photo, location, tub) VALUES ('p001.jpg', 'Aberdeen', NULL),
 ('p020.jpg', 'Bridge of Don', ''),
 ('p021.jpg', 'Bridge of Don', ''),
 ('p022.jpg', 'Newmacher', ''),
-('p024.jpg', 'Bridge of Don', '');
+('p024.jpg', 'Bridge of Don', ''),
+('p025.jpg', 'Stonehaven', NULL),
+('p026.jpg', 'Bucksburn', NULL),
+('p027.jpg', 'Bucksburn', NULL),
+('p028.jpg', 'Bucksburn', NULL);
 
 -- --------------------------------------------------------
 
@@ -129,13 +136,14 @@ CREATE TABLE series (
   series int(11) NOT NULL default '0',
   seriesName varchar(50) NOT NULL default '',
   PRIMARY KEY  (series)
-) ENGINE=MyISAM  COMMENT='Series';
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Series';
 
 --
 -- Dumping data for table series
 --
 
-INSERT INTO series (series, seriesName) VALUES (300, '300 Series'),
+INSERT INTO series (series, seriesName) VALUES
+(300, '300 Series'),
 (500, '500 Series'),
 (700, '700 Series');
 
@@ -173,7 +181,7 @@ CREATE TABLE tub (
   cust2 varchar(50) default NULL,
   PRIMARY KEY  (tub),
   KEY tub_series (series)
-) ENGINE=MyISAM ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table tub
@@ -185,7 +193,7 @@ INSERT INTO tub (series, tub, disabled, img_desc, colour, wood, tub_size, no_peo
 (500, '520', 0, 'Shown in Sterling', 'quartzite.jpg', 'twilight_enviroskirt.jpg', 'Small', '2+', '145 x 190 x 76 cm', '701 Litres', '193 kg', '894 kg', '14 locations selected to enable you to choose the type of therapy for your personalised body massage', 'Customise this 520 from a minimum of 18-42 different types of jetting combinations at no cost', 'High performing, efficient water cleaning system', 'Easy programming for heating and filtering to save money', 'Tight thermos bottle seal to save energy and money', 'Powerful 1.5 hp pump with stainless steel heater', 'Built to the highest standards available', 'Mood setting and safety for entry and exit', 'A no-fine-print, straightforward customer guarantee', NULL, NULL, NULL, NULL),
 (500, '530', 0, 'Shown in Tropical Mist', 'quartzite.jpg', 'walnut_enviroskirt.jpg', 'Medium', '6-7', '179 x 218 x 91 cm', '1000 Litres', '273 kg', '1273 kg', '15 locations selected to enable you to choose the type of therapy for your personalised body massage', 'Customise this 530 from a minimum of 15-70 different types of jetting combinations at no cost', 'High performing, efficient water cleaning system', 'Easy programming for heating and filtering to save money', 'Tight thermos bottle seal to save energy and money', 'Powerful 2.5 hp pump with stainless steel heater', 'Built to the highest standards available', 'Mood setting and safety for entry and exit', 'A no-fine-print, straightforward customer guarantee', NULL, NULL, NULL, NULL),
 (500, '538', 0, 'Shown in Graphite', 'tungsten.jpg', 'walnut_enviroskirt.jpg', 'Large', '8-9', '203 x 224 x 91 cm', '1363 Litres', '295 kg', '1658 kg', '15 locations selected to enable you to choose the type of therapy for your personalised body massage', 'Customise this 550 from a minimum of 15-70 different types of jetting combinations at no cost', 'High performing, efficient water cleaning system', 'Easy programming for heating and filtering to save money', 'Tight thermos bottle seal to save energy and money', 'Powerful 2.5 hp pump with stainless steel heater', 'Built to the highest standards available', 'Mood setting and safety for entry and exit', 'A no-fine-print, straightforward customer guarantee', 'When we use our Beachcomber Hot Tub, it''s one of the few times we get to relax and talk together.  We look forward to a private time in the tub.  No distractions, just us and our dreams.', 'Lyndon & Heather Thorness', 'Thank you for the return call a while back.  I ordered your video, but it wasn''t the video that sold me.  It was your attitude towards your customers.  We took delivery of our new Beachcomber Hot Tub and took our first soak Christmas eve.  What an enjoyable experience!  As a matter of fact, we haven''t missed a night yet.  We expect to get years of enjoyment from our investment.', 'Don & Lou Fornsdahl'),
-(500, '550x', 0, 'Shown in Azure', 'glacier.jpg', 'sequoia_enviroskirt.jpg', 'Large', '8-9', '203 x 224 x 91 cm', '1363 Litres', '295 kg', '1658 kg', '20 locations selected to enable you to choose the type of therapy for your personalised body massage', 'Customise this 550x from a minimum of 24-90 different types of jetting combinations at no cost', 'High performing, efficient water cleaning system', 'Easy programming for heating and filtering to save money', 'Tight thermos bottle seal to save energy and money', 'Powerful 3.5 hp pump with stainless steel heater', 'Built to the highest standards available', 'Mood setting and safety for entry and exit', 'A no-fine-print, straightforward customer guarantee', NULL, NULL, NULL, NULL),
+(500, '550x', 0, 'Shown in Azure', 'glacier.jpg', 'twilight_enviroskirt.jpg', 'Large', '8-9', '203 x 224 x 91 cm', '1363 Litres', '295 kg', '1658 kg', '20 locations selected to enable you to choose the type of therapy for your personalised body massage', 'Customise this 550x from a minimum of 24-90 different types of jetting combinations at no cost', 'High performing, efficient water cleaning system', 'Easy programming for heating and filtering to save money', 'Tight thermos bottle seal to save energy and money', 'Powerful 3.5 hp pump with stainless steel heater', 'Built to the highest standards available', 'Mood setting and safety for entry and exit', 'A no-fine-print, straightforward customer guarantee', NULL, NULL, NULL, NULL),
 (500, '580', 0, 'Shown in Garnet', 'carbon.jpg', 'ebony_enviroskirt.jpg', 'Large', '8-10', '226 x 226 x 91 cm', '1515 Litres', '329 kg', '1844 kg', '25 locations selected to enable you to choose the type of therapy for your personalised body massage', 'Customise this 580 from a minimum of 29-100 different types of jetting combinations at no cost', 'High performing, efficient water cleaning system', 'Easy programming for heating and filtering to save money', 'Tight thermos bottle seal to save energy and money', 'Powerful 4.5 hp pump with stainless steel heater', 'Built to the highest standards available', 'Mood setting and safety for entry and exit', 'A no-fine-print, straightforward customer guarantee', NULL, NULL, NULL, NULL),
 (700, '720', 0, 'Shown in Azure', 'sandstone.jpg', 'walnut_enviroskirt.jpg', 'Medium', '7-8', '203 x 224 x 91 cm', '1325 Litres', '307 kg', '1632 kg', '19 locations selected to enable you to choose the type of therapy for your personalised body massage', 'Customise this 720 from a minimum of 21-98 different types of jetting combinations at no cost', 'High performing, efficient water cleaning system', 'Easy programming for heating and filtering to save money', 'Tight thermos bottle seal to save energy and money', 'Powerful twin 1.5 hp pumps with stainless steel heater', 'Built to the highest standards available', 'Mood setting and safety for entry and exit', 'A no-fine-print, straightforward customer guarantee', NULL, NULL, NULL, NULL),
 (700, '720x', 0, 'Shown in Denim', 'beachwood.jpg', 'twilight_enviroskirt.jpg', 'Medium', '7-8', '203 x 224 x 91 cm', '1325 Litres', '307 kg', '1632 kg', '27 locations selected to enable you to choose the type of therapy for your personalised body massage', 'Customise this 720x from a minimum of 32-139 different types of jetting combinations at no cost', 'High performing, efficient water cleaning system', 'Easy programming for heating and filtering to save money', 'Tight thermos bottle seal to save energy and money', 'Powerful 1.5 and 3.5 hp pumps with stainless steel heater', 'Built to the highest standards available', 'Mood setting and safety for entry and exit', 'A no-fine-print, straightforward customer guarantee', NULL, NULL, NULL, NULL),

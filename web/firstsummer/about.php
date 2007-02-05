2d178e89c68b3336e02fa93ed01e6785e1dbbca8
craig <none@none>
2007-02-05 19:22:31 +0000

    10074

--- web/firstsummer/about.php
+++ web/firstsummer/about.php
@@ -1,7 +1,7 @@
 <?
 /* * * * * * * * * * * * * * * * * * * * * * * *
  * First Summerhouses
- * Copyright (c) 2006 Frontburner
+ * Copyright (c) 2006-2007 Frontburner
  * Author Craig McKay <craig@frontburner.co.uk>
  *
  * About Us page
@@ -10,6 +10,7 @@
  *
  * Who  When         Why
  * CAM  02-Oct-2006  10003 : File created.
+ * CAM  05-Feb-2007  10074 : Updated with new text and image.
  * * * * * * * * * * * * * * * * * * * * * * * */
 
 $title = "About Us";
@@ -19,10 +20,22 @@ include 'tpl/top.php';
 <table height="100%" width="100%" border=0 cellpadding=10 cellspacing=0>
 <tr>
   <td align=center>
-    <h3>1st Summerhouses is a family business based near Glasgow.</h3>
 
-    <p>We have established a distribution network that delivers throughout the UK and Europe.
-    We pride ourselves in our friendly, personal service and being able to offer our customers value for money.</p></td>
+    <h3>1st Summerhouses is a family run business that sells all
+    types of timber buildings, from Garden Rooms and Offices to Homes
+    and Holiday Accommodation.  We are based near Glasgow in Scotland.</h3>
+
+  </td>
+</tr><tr>
+  <td align=center><img width="350" height="262" src="img/aboutus1.jpg"></td>
+</tr><tr>
+  <td align=center>
+
+    <p>We have established a distribution network that delivers
+    throughout the UK and Europe. We pride ourselves in our friendly,
+    personal service and being able to offer our customers value for money.</p>
+
+  </td>
 </tr>
 </table>
 

4b3496fd9d853f3d9e4a088ab3f75028446061f9
craig <none@none>
2007-07-29 19:41:21 +0000

    Added a folder remotely

---
+++ web/_webframe/index.php
@@ -0,0 +1,18 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * WEBSITE_NAME
+ * Copyright (c) 2007 frontburner.co.uk
+ *
+ * Home Page Context
+ *
+ * $Id: /web/cc/index.php $
+ *
+ * Who  When         Why
+ * CAM  27-Dec-2005  File created.
+ * * * * * * * * * * * * * * * * * * * * * * * */
+
+$title = "Home";
+include 'tpl/top.php';
+include 'tpl/home.php';
+include 'tpl/bot.php';
+?>
---
+++ web/_webframe/task.php
@@ -0,0 +1,15 @@
+<?
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * WEBSITE_NAME
+ * Copyright (c) 2007 frontburner.co.uk
+ *
+ * Standard Database Tasks
+ *
+ * $Id: /web/cc/task.php $
+ *
+ * Who  When         Why
+ * CAM  27-Dec-2005  File created.
+ * * * * * * * * * * * * * * * * * * * * * * * */
+
+?>
+

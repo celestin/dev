bdad1143cbb89aed71c628616a372bde3016293d
craig <none@none>
2007-07-31 22:34:36 +0000

    10152

--- web/asrc.biz/data/diff.sql
+++ web/asrc.biz/data/diff.sql
@@ -11,8 +11,6 @@
 -- CAM  23-Jul-2007  10152 : Added Court Online dates.
 -- --------------------------------------------------------
 
-UPDATE member SET password='5f4dcc3b5aa765d61d8327deb882cf99';
-
 REPLACE INTO member_type (member_type, type_name, comments1, comments2, valid, limits, disp_order) VALUES 
 ('Y', 'Unspecified', NULL, NULL, NULL, NULL, '0'), 
 ('Z', 'Unknown', NULL, NULL, NULL, NULL, '0');

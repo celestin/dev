666db2587f494614e8a67ad19c6b3199bd995f3d
craig <none@none>
2007-10-15 20:10:07 +0000

    Removed old source.

--- web/ministry/ministry_del_afterwards/_vti_cnf/fn.js
+++
@@ -1,8 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|05 Feb 2006 14:02:18 -0000
-vti_extenderversion:SR|5.0.2.2634
-vti_usagebymonth:UX|2 2
-vti_usagebyweek:UX|0 1 0 1 1
-vti_usagebyday:UX|0 1
-vti_usagelastupdated:TX|21 May 2006 03:02:42 -0000
-vti_usagetotalhits:IX|4
--- web/ministry/ministry_del_afterwards/_vti_cnf/top.php
+++
@@ -1,8 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|01 Oct 2003 22:36:04 -0000
-vti_extenderversion:SR|5.0.2.2623
-vti_usagebymonth:UX|0 0 0 0 0 0 0 1 0 0 8 3 1
-vti_usagebyweek:UX|0
-vti_usagebyday:UX|0
-vti_usagelastupdated:TX|01 May 2006 03:04:16 -0000
-vti_usagetotalhits:IX|13

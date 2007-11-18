28a9dc73584a68ee599ce08dddc6e3f07ad98a34
craig <none@none>
2007-11-18 11:01:40 +0000

    10156

--- web/asrc.biz/tpl/home.php
+++ web/asrc.biz/tpl/home.php
@@ -1,8 +1,7 @@
 <?php
 /* * * * * * * * * * * * * * * * * * * * * * * *
  * ASRC.biz (Aberdeen Squash Racquets Club)
- *
- * Copyright (c) 2006 Frontburner
+ * Copyright (c) 2006-2007 Frontburner
  * Author Craig McKay <craig@frontburner.co.uk>
  *
  * Bottom of the page
@@ -11,10 +10,11 @@
  *
  * Who  When         Why
  * CAM  19-Mar-2004  File created.
+ * CAM  17-Nov-2007  10156 : Updated Monitor logo.
  * * * * * * * * * * * * * * * * * * * * * * * */
 ?>
 
 <table height="100%" width="100%" border=0>
-<tr><td align=center><img height="186" width="500" src="img/home.gif"></td></tr>
+<tr><td align=center><img height="186" width="500" src="img/home2.gif"></td></tr>
 </table>
 

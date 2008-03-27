ab4d8ab142a28eed898cc7bea3883c97562983b7
craig <none@none>
2008-03-27 23:16:53 +0000

    347

---
+++ ps/krakatau/metrics/Run/Run.h
@@ -0,0 +1,61 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Krakatau Application Launcher
+ * Copyright (c) 2000,2008 Power Software
+ * Author Craig McKay <craig@frontburner.co.uk>
+ *
+ * $Id$
+ *
+ * Who  When       Why
+ * CAM  27-Mar-08  347 : File added to source control.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#if !defined(AFX_RUN_H__B3AE182E_D046_11D4_B675_0050DA35E990__INCLUDED_)
+#define AFX_RUN_H__B3AE182E_D046_11D4_B675_0050DA35E990__INCLUDED_
+
+#include <process.h>
+#include <windows.h>
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#ifndef __AFXWIN_H__
+  #error include 'stdafx.h' before including this file for PCH
+#endif
+
+#include "resource.h"   // main symbols
+
+/////////////////////////////////////////////////////////////////////////////
+// CRunApp:
+// See Run.cpp for the implementation of this class
+//
+
+/*
+class CRunApp : public CWinApp
+{
+public:
+  CRunApp();
+
+// Overrides
+  // ClassWizard generated virtual function overrides
+  //{{AFX_VIRTUAL(CRunApp)
+  public:
+  virtual BOOL InitInstance();
+  //}}AFX_VIRTUAL
+
+// Implementation
+
+  //{{AFX_MSG(CRunApp)
+    // NOTE - the ClassWizard will add and remove member functions here.
+    //    DO NOT EDIT what you see in these blocks of generated code !
+  //}}AFX_MSG
+  DECLARE_MESSAGE_MAP()
+};
+*/
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_RUN_H__B3AE182E_D046_11D4_B675_0050DA35E990__INCLUDED_)
---
+++ ps/krakatau/metrics/Run/StdAfx.h
@@ -0,0 +1,33 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Krakatau Application Launcher
+ * Copyright (c) 2000,2008 Power Software
+ * Author Craig McKay <craig@frontburner.co.uk>
+ *
+ * $Id$
+ *
+ * Who  When       Why
+ * CAM  27-Mar-08  347 : File added to source control.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#if !defined(AFX_STDAFX_H__B3AE1832_D046_11D4_B675_0050DA35E990__INCLUDED_)
+#define AFX_STDAFX_H__B3AE1832_D046_11D4_B675_0050DA35E990__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define VC_EXTRALEAN    // Exclude rarely-used stuff from Winhndows headers
+
+#include <afxwin.h>         // MFC core and standard components
+#include <afxext.h>         // MFC extensions
+#include <afxdisp.h>        // MFC Automation classes
+#include <afxdtctl.h>   // MFC support for Internet Explorer 4 Common Controls
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>     // MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__B3AE1832_D046_11D4_B675_0050DA35E990__INCLUDED_)

8664a8b78415a8f1ae319f3ef3b4149bea810ec2
craig <none@none>
2008-05-25 12:24:56 +0000

    362

--- ps/kepm/KrakatauEPM/Program.cs
+++ ps/kepm/KrakatauEPM/Program.cs
@@ -10,8 +10,6 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 using System.Diagnostics;
 

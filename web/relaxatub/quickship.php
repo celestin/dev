d7a2da92a755d2c46da2976cc15e79830514d109
craig <none@none>
2008-11-08 11:48:10 +0000

    10237

---
+++ web/relaxatub/quickship.php
@@ -0,0 +1,117 @@
+<?
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * Relaxatub
+ * Copyright (c) 2008 Frontburner
+ * Author Craig McKay <craig@frontburner.co.uk>
+ *
+ * Quick Ship
+ *
+ * $Id$
+ *
+ * Who  When         Why
+ * CAM  08-Nov-2008  10237 : File created.
+ * * * * * * * * * * * * * * * * * * * * * * * */
+
+$pageId = "pagequickship";
+$pageTitle = "Quick Ship";
+include "tpl/top.php";
+?>
+<div id="quickship">
+
+<p>These products can all be shipped quickly - call us for more information 01224 580792!</p>
+
+<table border=0 cellpadding=0 cellspacing=8 align=left width="100%">
+
+<tr>
+  <th>Model</th>
+  <th>Acrylic Colour</th>
+  <th>Cabinet Colour</th>
+  <th>Seats</th>
+  <th>Details</th>
+</tr>
+
+<tr>
+  <td>310</td>
+  <td><img src="img/col/2008/quartzite.png"></td>
+  <td><img src="img/col/2008/walnut_enviro.png"></td>
+  <td>2</td>
+  <td><a href="tub.php?tub=310"><img border=0 src="img/tub/fa/310.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>320X</td>
+  <td><img src="img/col/2008/atlantis.png"></td>
+  <td><img src="img/col/2008/natural_enviro.png"></td>
+  <td>4&nbsp;-&nbsp;5</td>
+  <td><a href="tub.php?tub=320x"><img border=0 src="img/tub/fa/320x.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>350</td>
+  <td><img src="img/col/2008/sterling.png"></td>
+  <td><img src="img/col/2008/ebony_enviro.png"></td>
+  <td>6</td>
+  <td><a href="tub.php?tub=350"><img border=0 src="img/tub/fa/350h.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>360 BLE</td>
+  <td><img src="img/col/2008/cobalt.png"></td>
+  <td><img src="img/col/2008/twilight_enviro.png"></td>
+  <td>6&nbsp;+&nbsp;1</td>
+  <td><a href="tub.php?tub=360"><img border=0 src="img/tub/fa/360.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>378 BLE</td>
+  <td><img src="img/col/2008/alabaster.png"></td>
+  <td><img src="img/col/2008/walnut_enviro.png"></td>
+  <td>7&nbsp;+&nbsp;1</td>
+  <td><a href="tub.php?tub=378"><img border=0 src="img/tub/fa/378.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>530</td>
+  <td><img src="img/col/2008/tungsten.png"></td>
+  <td><img src="img/col/2008/walnut_enviro.png"></td>
+  <td>5&nbsp;+&nbsp;2</td>
+  <td><a href="tub.php?tub=530"><img border=0 src="img/tub/fa/530.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>578</td>
+  <td><img src="img/col/2008/cobalt.png"></td>
+  <td><img src="img/col/2008/ebony_enviro.png"></td>
+  <td>7&nbsp;+&nbsp;1</td>
+  <td><a href="tub.php?tub=578"><img border=0 src="img/tub/fa/578.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>720x</td>
+  <td><img src="img/col/2008/quartzite.png"></td>
+  <td><img src="img/col/2008/walnut_enviro.png"></td>
+  <td>6&nbsp;+&nbsp;1</td>
+  <td><a href="tub.php?tub=720x"><img border=0 src="img/tub/fa/720x.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>720<p>with music</p></td>
+  <td><img src="img/col/2008/carbon.png"></td>
+  <td><img src="img/col/2008/ebony_enviro.png"></td>
+  <td>7&nbsp;+&nbsp;2</td>
+  <td><a href="tub.php?tub=720"><img border=0 src="img/tub/fa/720.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>750E</td>
+  <td><img src="img/col/2008/alabaster.png"></td>
+  <td><img src="img/col/2008/ebony_enviro.png"></td>
+  <td>8&nbsp;+&nbsp;1</td>
+  <td><a href="tub.php?tub=750"><img border=0 src="img/tub/fa/750.jpg" height="84"></a></td>
+</tr>
+
+</table>
+</div>
+<?
+  include "tpl/bot.php";
+?>

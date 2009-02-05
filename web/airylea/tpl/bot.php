c2a9c6c268a2abe264daf31dbc5dc38acaad72f0
craig <none@none>
2009-02-05 21:04:35 +0000

    10280

--- web/airylea/tpl/bot.php
+++ web/airylea/tpl/bot.php
@@ -10,10 +10,11 @@
  *
  * Who  When         Why
  * CAM  28-Aug-2008  10280 : File created.
+ * CAM  05-Feb-2009  10280 : Added auto copyright year.
  * * * * * * * * * * * * * * * * * * * * * * * */
 ?>
 </td></tr>
-<tr><td colspan=2 class="cpyrght"><br>Copyright &copy; 2008 Airylea Motors. All rights reserved.
+<tr><td colspan=2 class="cpyrght"><br>Copyright &copy; <? echo strftime("%Y"); ?> Airylea Motors. All rights reserved.
 <br>Site designed and maintained by <a href="http://www.frontburner.co.uk/">frontburner.co.uk</a></td></tr>
 </table></td>
 </tr></table>

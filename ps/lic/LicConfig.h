047948f01f0c40adf3c63f6bcab6bfdd7088473b
craig <none@none>
2009-06-24 21:55:38 +0000

    #10449

--- ps/lic/LicConfig.h
+++ ps/lic/LicConfig.h
@@ -1,24 +1,25 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * FLEXlm License Verifier
- * Copyright 2004-2007 Power Software
+ * Copyright 2004,2009 Power Software
  * Author Craig McKay <craig@frontburner.co.uk>
  *
  * Configure the various licensing options
  *
  * $Id$
  *
- * Who  When       Why
- * CAM  08-Jan-05  File added.
- * CAM  07-May-05  Added variety of EPM languages.
+ * Who  When         Why
+ * CAM  08-Jan-05    File added.
+ * CAM  07-May-05    Added variety of EPM languages.
  * CAM  02-Aug-05    76 : Added ADA.
- * CAM  13-Aug-05   114 : Added Perl.
- * CAM  19-Sep-05   138 : Added KEPM.
- * CAM  27-Oct-06   117 : Added ASP.
- * CAM  04-Nov-06   301 : Added PHP.
- * CAM  28-Jun-07   314 : Added Ericsson IDL.
- * CAM  17-Jul-07   316 : Added VHDL.
- * CAM  04-Dec-07   324 : Added JSP/XML (#325).
- * CAM  26-Mar-09   10400 : Added AY/HT/JT/PY (#10400/10401/10402/10403).
+ * CAM  13-Aug-05    114 : Added Perl.
+ * CAM  19-Sep-05    138 : Added KEPM.
+ * CAM  27-Oct-06    117 : Added ASP.
+ * CAM  04-Nov-06    301 : Added PHP.
+ * CAM  28-Jun-07    314 : Added Ericsson IDL.
+ * CAM  17-Jul-07    316 : Added VHDL.
+ * CAM  04-Dec-07    324 : Added JSP/XML (#325).
+ * CAM  26-Mar-09    10400 : Added AY/HT/JT/PY (#10400/10401/10402/10403).
+ * CAM  24-Jun-2009  10449 : Added CSS Stylesheet (CS), Fortran (FT), Ruby (RB), Shell Script (SH), Text (TX) and Windows Batch File (WB).
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #define LICENSE_VERSION "1.0"
@@ -41,6 +42,12 @@
 #define LIC_HT   "epmht"
 #define LIC_JT   "epmjt"
 #define LIC_PY   "epmpy"
+#define LIC_CS   "epmcs"
+#define LIC_FT   "epmft"
+#define LIC_RB   "epmrb"
+#define LIC_SH   "epmsh"
+#define LIC_TX   "epmtx"
+#define LIC_WB   "epmwb"
 
 #define EPM_CS   1
 #define EPM_CP   2
@@ -60,4 +67,9 @@
 #define EPM_HT   32768
 #define EPM_JT   65536
 #define EPM_PY   131072
-
+#define EPM_CS   262144
+#define EPM_FT   524288
+#define EPM_RB   1048576
+#define EPM_SH   2097152
+#define EPM_TX   4194304
+#define EPM_WB   8388608
--- ps/lic/lic.cpp
+++ ps/lic/lic.cpp
@@ -1,26 +1,27 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * FLEXlm License Verifier
- * Copyright 2004-2007 Power Software
+ * Copyright 2004,2009 Power Software
  * Author Craig McKay <craig@frontburner.co.uk>
  *
  * Performs the FLEXlm check
  *
  * $Id$
  *
- * Who  When       Why
- * CAM  08-Jan-05  File added.
- * CAM  07-May-05  Added variety of EPM languages.
+ * Who  When         Why
+ * CAM  08-Jan-05    File added.
+ * CAM  07-May-05    Added variety of EPM languages.
  * CAM  02-Aug-05    76 : Added ADA.
- * CAM  09-Aug-05   110 : Correct license problems.
- * CAM  12-Aug-05   110 : Glorious Twelfth - Correct license problems.
- * CAM  13-Aug-05   114 : Added Perl.
- * CAM  19-Sep-05   138 : Added KEPM.
- * CAM  27-Oct-06   117 : Added ASP.
- * CAM  04-Nov-06   301 : Added PHP.
- * CAM  28-Jun-07   314 : Added Ericsson IDL.
- * CAM  17-Jul-07   316 : Added VHDL.
- * CAM  11-Dec-07   325 : Added JSP/XML.
- * CAM  26-Mar-09   10400 : Added AY/HT/JT/PY (#10400/10401/10402/10403).
+ * CAM  09-Aug-05    110 : Correct license problems.
+ * CAM  12-Aug-05    110 : Glorious Twelfth - Correct license problems.
+ * CAM  13-Aug-05    114 : Added Perl.
+ * CAM  19-Sep-05    138 : Added KEPM.
+ * CAM  27-Oct-06    117 : Added ASP.
+ * CAM  04-Nov-06    301 : Added PHP.
+ * CAM  28-Jun-07    314 : Added Ericsson IDL.
+ * CAM  17-Jul-07    316 : Added VHDL.
+ * CAM  11-Dec-07    325 : Added JSP/XML.
+ * CAM  26-Mar-09    10400 : Added AY/HT/JT/PY (#10400/10401/10402/10403).
+ * CAM  24-Jun-2009  10449 : Added CSS Stylesheet (CS), Fortran (FT), Ruby (RB), Shell Script (SH), Text (TX) and Windows Batch File (WB).
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include "LicConfig.h"
@@ -84,7 +85,13 @@ DWORD main(int argc, char* argv[]) {
   inc(EPM_HT, LIC_HT);
   inc(EPM_JT, LIC_JT);
   inc(EPM_PY, LIC_PY);
- 
+  inc(EPM_CS, LIC_CS);
+  inc(EPM_FT, LIC_FT);
+  inc(EPM_RB, LIC_RB);
+  inc(EPM_SH, LIC_SH);
+  inc(EPM_TX, LIC_TX);
+  inc(EPM_WB, LIC_WB);
+
   if (debug) cout << endl << "Total:" << rval << endl;
 
   return rval;

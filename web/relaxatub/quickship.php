aaa2b76481f772d7cfbfe26a8a7e105571f06092
craig <none@none>
2009-08-12 00:03:45 +0000

    #10471

--- web/relaxatub/quickship.php
+++ web/relaxatub/quickship.php
@@ -1,7 +1,7 @@
 <?
 /* * * * * * * * * * * * * * * * * * * * * * * *
  * Relaxatub
- * Copyright (c) 2008 Frontburner
+ * Copyright (c) 2008,2009 Frontburner
  * Author Craig McKay <craig@frontburner.co.uk>
  *
  * Quick Ship
@@ -10,6 +10,7 @@
  *
  * Who  When         Why
  * CAM  08-Nov-2008  10237 : File created.
+ * CAM  11-Aug-2009  10471 : Updated Quick Ship for 2009.
  * * * * * * * * * * * * * * * * * * * * * * * */
 
 $pageId = "pagequickship";
@@ -18,7 +19,7 @@ include "tpl/top.php";
 ?>
 <div id="quickship">
 
-<p>These products can all be shipped quickly - call us for more information 01224 580792!</p>
+<p>These products can all be shipped quickly - call us for more information 01224 875620!</p>
 
 <table border=0 cellpadding=0 cellspacing=8 align=center width="100%">
 
@@ -31,7 +32,7 @@ include "tpl/top.php";
 </tr>
 
 <tr>
-  <td>310</td>
+  <td>310<br /><span id="tubtype">Portable</span></td>
   <td><img src="img/col/2008/quartzite.png"></td>
   <td><img src="img/col/2008/walnut_enviro.png"></td>
   <td>2</td>
@@ -39,71 +40,95 @@ include "tpl/top.php";
 </tr>
 
 <tr>
-  <td>320X</td>
+  <td>321<br /><span id="tubtype">Portable</span></td>
   <td><img src="img/col/2008/atlantis.png"></td>
-  <td><img src="img/col/2008/natural_enviro.png"></td>
+  <td><img src="img/col/2008/twilight_enviro.png"></td>
   <td>4&nbsp;-&nbsp;5</td>
-  <td><a href="tub.php?tub=320x"><img border=0 src="img/tub/fa/320x.jpg" height="84"></a></td>
+  <td><a href="tub.php?tub=321"><img border=0 src="img/tub/fa/321.jpg" height="84"></a></td>
 </tr>
 
 <tr>
-  <td>350</td>
-  <td><img src="img/col/2008/sterling.png"></td>
+  <td>340<br /><span id="tubtype">Portable</span></td>
+  <td><img src="img/col/2008/opal.png"></td>
+  <td><img src="img/col/2008/ebony_enviro.png"></td>
+  <td>5</td>
+  <td><a href="tub.php?tub=340"><img border=0 src="img/tub/fa/340.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>350<br /><span id="tubtype">Portable</span></td>
+  <td><img src="img/col/2008/alabaster.png"></td>
   <td><img src="img/col/2008/ebony_enviro.png"></td>
   <td>6</td>
-  <td><a href="tub.php?tub=350"><img border=0 src="img/tub/fa/350h.jpg" height="84"></a></td>
+  <td><a href="tub.php?tub=350"><img border=0 src="img/tub/fa/350.jpg" height="84"></a></td>
 </tr>
 
 <tr>
-  <td>360 BLE</td>
-  <td><img src="img/col/2008/cobalt.png"></td>
-  <td><img src="img/col/2008/twilight_enviro.png"></td>
+  <td>350<br /><span id="tubtype">Hybrid</span></td>
+  <td><img src="img/col/2008/alabaster.png"></td>
+  <td><img src="img/col/2008/ebony_enviro.png"></td>
+  <td>6</td>
+  <td><a href="tub.php?tub=350"><img border=0 src="img/tub/fa/350.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>360<br /><span id="tubtype">Portable</span></td>
+  <td><img src="img/col/2008/quartzite.png"></td>
+  <td><img src="img/col/2008/walnut_enviro.png"></td>
   <td>6&nbsp;+&nbsp;1</td>
   <td><a href="tub.php?tub=360"><img border=0 src="img/tub/fa/360.jpg" height="84"></a></td>
 </tr>
 
 <tr>
-  <td>378 BLE</td>
+  <td>360<br /><span id="tubtype">Hybrid</span></td>
+  <td><img src="img/col/2008/cobalt.png"></td>
+  <td><img src="img/col/2008/ebony_enviro.png"></td>
+  <td>6&nbsp;+&nbsp;1</td>
+  <td><a href="tub.php?tub=360"><img border=0 src="img/tub/fa/360.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>380<br /><span id="tubtype">Hybrid</span></td>
   <td><img src="img/col/2008/alabaster.png"></td>
   <td><img src="img/col/2008/walnut_enviro.png"></td>
   <td>7&nbsp;+&nbsp;1</td>
-  <td><a href="tub.php?tub=378"><img border=0 src="img/tub/fa/378.jpg" height="84"></a></td>
+  <td><a href="tub.php?tub=380"><img border=0 src="img/tub/fa/380.jpg" height="84"></a></td>
 </tr>
 
 <tr>
-  <td>530</td>
-  <td><img src="img/col/2008/tungsten.png"></td>
+  <td>550<br /><span id="tubtype">Hybrid</span></td>
+  <td><img src="img/col/2008/alabaster.png"></td>
   <td><img src="img/col/2008/walnut_enviro.png"></td>
-  <td>5&nbsp;+&nbsp;2</td>
-  <td><a href="tub.php?tub=530"><img border=0 src="img/tub/fa/530.jpg" height="84"></a></td>
+  <td>6&nbsp;+&nbsp;2</td>
+  <td><a href="tub.php?tub=550"><img border=0 src="img/tub/fa/550.jpg" height="84"></a></td>
 </tr>
 
 <tr>
-  <td>578</td>
-  <td><img src="img/col/2008/cobalt.png"></td>
-  <td><img src="img/col/2008/ebony_enviro.png"></td>
+  <td>578<br /><span id="tubtype">Hybrid</span></td>
+  <td><img src="img/col/2008/quartzite.png"></td>
+  <td><img src="img/col/2008/walnut_enviro.png"></td>
   <td>7&nbsp;+&nbsp;1</td>
   <td><a href="tub.php?tub=578"><img border=0 src="img/tub/fa/578.jpg" height="84"></a></td>
 </tr>
 
 <tr>
-  <td>720x</td>
-  <td><img src="img/col/2008/quartzite.png"></td>
-  <td><img src="img/col/2008/walnut_enviro.png"></td>
-  <td>6&nbsp;+&nbsp;1</td>
-  <td><a href="tub.php?tub=720x"><img border=0 src="img/tub/fa/720x.jpg" height="84"></a></td>
-</tr>
-
-<tr>
-  <td>720<p>with music</p></td>
-  <td><img src="img/col/2008/carbon.png"></td>
+  <td>720<br /><span id="tubtype">Hybrid</span></td>
+  <td><img src="img/col/2008/alabaster.png"></td>
   <td><img src="img/col/2008/ebony_enviro.png"></td>
   <td>7&nbsp;+&nbsp;2</td>
   <td><a href="tub.php?tub=720"><img border=0 src="img/tub/fa/720.jpg" height="84"></a></td>
 </tr>
 
 <tr>
-  <td>750E</td>
+  <td>725<br /><span id="tubtype">Hybrid</span></td>
+  <td><img src="img/col/2008/quartzite.png"></td>
+  <td><img src="img/col/2008/walnut_enviro.png"></td>
+  <td>6&nbsp;+&nbsp;1</td>
+  <td><a href="tub.php?tub=725"><img border=0 src="img/tub/fa/725.jpg" height="84"></a></td>
+</tr>
+
+<tr>
+  <td>750E<br /><span id="tubtype">Hybrid</span></td>
   <td><img src="img/col/2008/alabaster.png"></td>
   <td><img src="img/col/2008/ebony_enviro.png"></td>
   <td>8&nbsp;+&nbsp;1</td>

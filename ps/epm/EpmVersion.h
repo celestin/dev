053f47f47d99384437d74bbfa73b7e721a0c1b0c
craig <none@none>
2009-10-27 21:15:04 +0000

    #10499

--- ps/epm/EpmVersion.h
+++ ps/epm/EpmVersion.h
@@ -10,6 +10,7 @@
  * Who  When         Why
  * CAM  27-Aug-2009  10483 : Version 1.19.1.0.
  * CAM  15-Sep-2009  10490 : Version 1.19.2.0.
+ * CAM  27-Oct-2009  10499 : Version 1.19.3.0.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-#define EPM_VERSION "1.19.2.0"
+#define EPM_VERSION "1.19.3.0"

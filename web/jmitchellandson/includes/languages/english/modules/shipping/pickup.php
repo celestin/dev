05de1ec72d967211cdd782c83934a535266abe47
craig <none@none>
2010-12-28 18:55:36 +0000

    #10910

---
+++ web/jmitchellandson/includes/languages/english/modules/shipping/pickup.php
@@ -0,0 +1,22 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * J.Mitchell & Son (jmitchellandson.co.uk)
+ *
+ * osCommerce, Open Source E-Commerce Solutions
+ * http://www.oscommerce.com
+ * Copyright (c) 2007 osCommerce
+ * Released under the GNU General Public License
+ *
+ * Customised by Front Burner
+ * Author Craig McKay <craig@frontburner.co.uk>
+ *
+ * $Id: $
+ *
+ * Who  When         Why
+ * CAM  28-Dec-2010  10910 : File added (http://www.oscommerce.com/community/contributions,598).
+ * * * * * * * * * * * * * * * * * * * * * * * */
+
+  define('MODULE_SHIPPING_PICKUP_TEXT_TITLE', 'Pickup from Shop');
+  define('MODULE_SHIPPING_PICKUP_TEXT_DESCRIPTION', 'Pickup');
+  define('MODULE_SHIPPING_PICKUP_TEXT_WAY', 'Collect your order from our shop');
+?>

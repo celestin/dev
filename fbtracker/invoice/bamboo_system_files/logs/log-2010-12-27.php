f5668a1e446517db3c8ddd21127d8b629765a591
craig <none@none>
2010-12-30 18:45:11 +0000

    #10912

---
+++ fbtracker/invoice/bamboo_system_files/logs/log-2010-12-27.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2010-12-27 13:34:28 --> Severity: Warning  --> mysql_connect(): Access denied for user 'root'@'localhost' (using password: YES) C:\csvn\www\bamboo\bamboo_system_files\database\drivers\mysql\mysql_driver.php 70
+ERROR - 2010-12-27 13:34:28 --> Unable to connect to the database
+ERROR - 2010-12-27 16:15:29 --> Severity: 8192  --> Function set_magic_quotes_runtime() is deprecated C:\csvn\www\bamboo\bamboo_system_files\plugins\dompdf\lib\class.pdf.php 4332
+ERROR - 2010-12-27 16:15:29 --> Severity: 8192  --> Function set_magic_quotes_runtime() is deprecated C:\csvn\www\bamboo\bamboo_system_files\plugins\dompdf\lib\class.pdf.php 4348
+ERROR - 2010-12-27 16:16:45 --> You did not select a file to upload.
+ERROR - 2010-12-27 16:17:00 --> You did not select a file to upload.
+ERROR - 2010-12-27 16:17:18 --> You did not select a file to upload.
+ERROR - 2010-12-27 16:17:27 --> Severity: 8192  --> Function set_magic_quotes_runtime() is deprecated C:\csvn\www\bamboo\bamboo_system_files\plugins\dompdf\lib\class.pdf.php 4332
+ERROR - 2010-12-27 16:17:27 --> Severity: 8192  --> Function set_magic_quotes_runtime() is deprecated C:\csvn\www\bamboo\bamboo_system_files\plugins\dompdf\lib\class.pdf.php 4348

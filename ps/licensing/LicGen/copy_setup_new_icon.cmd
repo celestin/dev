3b616a0659eafa5c3597aa4c3378086ad548ec03
craig <none@none>
2011-10-01 09:16:30 +0000

    #11036

--- ps/licensing/LicGen/copy_setup_new_icon.cmd
+++ ps/licensing/LicGen/copy_setup_new_icon.cmd
@@ -5,8 +5,8 @@ echo $Id$
 
 set SETUPFILE=setup.exe
 set INSTALLFILE=PowerSoftwareLicGen.exe
-set OUTPUT_PATH=C:\Dev\PS\licensing\EPMLicGen\EPMLicGen\Express\SingleImage\DiskImages\DISK1
-set ICON=C:\Dev\PS\licensing\EPMLicGen\LicenseKey.ico
+set OUTPUT_PATH=C:\Dev\PS\licensing\LicGen\LicGen\Express\SingleImage\DiskImages\DISK1
+set ICON=C:\Dev\PS\licensing\LicGen\LicenseKey.ico
 set ZIPFILE=licgen_303_0000.zip
 
 echo Copying output SETUP.EXE and replacing Icon
@@ -20,4 +20,3 @@ zip %ZIPFILE% %INSTALLFILE%
 echo Deleting temp exe 
 
 del %INSTALLFILE%
-

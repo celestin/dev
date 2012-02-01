e65cd81583dc0ee377daef62685947077e305247
craig <none@none>
2012-02-01 21:51:38 +0000

    #11084

--- web/towingdirect/towing.php
+++ web/towingdirect/towing.php
@@ -10,6 +10,7 @@
  *
  * Who  When         Why
  * CAM  23-Jan-2012  11085 : Page created.
+ * CAM  01-Feb-2012  11084 : Text added.
  * * * * * * * * * * * * * * * * * * * * * * * */
 
 $title = "Towing Direct";
@@ -22,8 +23,19 @@ showFlash();
 ?><table border=0 cellpadding=0 cellspacing=0 width="100%">
 <tr>
   <td colspan=2 width=700>
-    <h3>Car Transport in Scotland and the UK</h3>
+    <h3>We can provide a towing service from and to anywhere in Europe</h3>
 
+		<p>
+			<ul>
+				<li><b>Caravans</b> &mdash; Home to site and back. Ideal for Caravan Dealers</li>
+				<li><b>Cars</b> &mdash; Classic and vintage cars moved on your own trailer or ours</li>
+				<li><b>Event Trailers</b> &mdash; We can collect and install at events your trailer also we have the facilities to collect and deliver promotional material and have your trailer ready for the big event</li>
+				<li><b>Livestock Trailers</b> &mdash; Whatever your needs we can adapt to give you peace of mind</li>
+				<li><b>Conditions</b> &mdash; All trailers must be roadworthy with brakes, tyres and lighting all in accordance with road regulations.
+                                                Any expense incurred as a result of failure with the trailer will be charged to customer.
+                                                All trailers must not be more than 3.5t gross plated weight.</li>
+     </ul>
+    </p>
   </td>
 </tr>
 

5e3748561e39399a79567f77267748e1e91bc564
craig <none@none>
2012-10-16 20:30:20 +0000

    #11140

--- apps/dev/TMAX/Apps/WmsUser/users/identifysite.php
+++ apps/dev/TMAX/Apps/WmsUser/users/identifysite.php
@@ -10,6 +10,7 @@
  * Who  When         Why
  * CAM  08-Oct-2012  11128 : File created.
  * CAM  14-Oct-2012  11136 : Replaced deprecated session_is_registered call.
+ * CAM  16-Oct-2012  11140 : Added % complete as a figure, and a total line with total % complete.
  * * * * * * * * * * * * * * * * * * * * * * * */
 
 $title = "Identify Site";
@@ -24,9 +25,9 @@ $loggedin = (isset($_SESSION['member_person']) && isset($_SESSION['Talisman WMS 
 
 function supervisorlink($site, $supervisor) {
   global $cfg;
-  
+
   if (empty($supervisor)) return "&nbsp;";
-  
+
   return "<a href=\"" . $cfg['Site']['URL'] . "/identify.php?site=$site&username=$supervisor&cid=". md5($site . $supervisor) ."\">".$supervisor."</a>";
 }
 
@@ -35,7 +36,7 @@ function ragprogress($value) {
   $colour = "red";
   if ($value >= 75) $colour = "green";
   else if ($value >=25) $colour = "amber";
-  return "<img src=\"img/progress_$colour.jpg\" width=\"$width\" height=\"14\">";
+  return "<img style=\"padding-top:2px;\" src=\"img/progress_$colour.jpg\" width=\"$width\" height=\"12\">";
 }
 
 $site = NULL;      if (!empty($_GET['site'])) $site = $_GET['site'];
@@ -67,7 +68,7 @@ if ($loggedin) {
 
 <?
   $ssql =
-    "SELECT site, SiteFullName, InitCount, ConfirmedCount, InitCount+ConfirmedCount TotalCount, (ConfirmedCount/(InitCount+ConfirmedCount))*100 PctComplete, memberid ".
+    "SELECT site, SiteFullName, InitCount, ConfirmedCount, InitCount+ConfirmedCount TotalCount, FLOOR(ConfirmedCount/(InitCount+ConfirmedCount)*100) PctComplete, memberid ".
     "FROM ( ".
     "SELECT u.site, MIN(s.SiteFullName) SiteFullName, sum(InitCount) InitCount, sum(ConfirmedCount) ConfirmedCount, MIN(m.memberid) memberid ".
     "FROM ( ".
@@ -88,21 +89,35 @@ if ($loggedin) {
     "ORDER BY SiteFullName";
   $sql = mysql_query($ssql) or die (mysql_error());
 
+  $total_init = $total_conf = 0;
+
   while ($row = mysql_fetch_array($sql)) {
     $rowid_main = $row[0] . "_a";
 ?>
     <tr>
       <td><?=nvhtml($row[1])?></td>
-      <td><?=nvhtml($row[2])?></td>
-      <td><?=nvhtml($row[3])?></td>
-      <td><?=nvhtml($row[4])?></td>
-      <td><?=ragprogress($row[5])?></td>
+      <td class="num"><?=nvhtml($row[2])?></td>
+      <td class="num"><?=nvhtml($row[3])?></td>
+      <td class="num"><?=nvhtml($row[4])?></td>
+      <td valign="center"><?=ragprogress($row[5])?> <?=$row[5]?>%</td>
       <td><?=supervisorlink($row[0], $row[6])?></td>
     </tr>
     <?
+
+    $total_init += $row[2];
+    $total_conf += $row[3];
   }
 
+  $total_pct = floor($total_conf/($total_init+$total_conf)*100);
 ?>
+    <tr>
+      <td class="num"><b>Total</b></td>
+      <td class="num"><?=$total_init?></td>
+      <td class="num"><?=$total_conf?></td>
+      <td class="num"><?=$total_init+$total_conf?></td>
+      <td valign="center"><?=ragprogress($total_pct)?> <?=$total_pct?>%</td>
+      <td><?=supervisorlink($row[0], $row[6])?></td>
+    </tr>
 </table>
 <?
 include 'tpl/bot.php';
